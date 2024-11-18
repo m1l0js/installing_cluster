fb090af27ed0dd2ce6a4303763e9b92d2cbd5e70
m1l0js <m1l0js@proton.me>
2024-11-18 02:24:46 +0100

    Update kubeadm_setup.sh

--- kubeadm_setup.sh
+++ kubeadm_setup.sh
@@ -2,8 +2,8 @@
 
 function networking {
     # sysctl params required by setup, params persist across reboots
-    cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-    net.ipv4.ip_forward = 1
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.ipv4.ip_forward = 1
 EOF
 
     # Apply sysctl params without reboot
@@ -25,23 +25,23 @@ function install-crio {
     CRIO_VERSION=v1.31
 
     # Add the Kubernetes repository
-    cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
-    [kubernetes]
-    name=Kubernetes
-    baseurl=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/
-    enabled=1
-    gpgcheck=1
-    gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd.xml.key
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/$KUBERNETES_VERSION/rpm/repodata/repomd.xml.key
 EOF
 
     # Add the CRI-O repository
-    cat <<EOF | tee /etc/yum.repos.d/cri-o.repo
-    [cri-o]
-    name=CRI-O
-    baseurl=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/
-    enabled=1
-    gpgcheck=1
-    gpgkey=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/repodata/repomd.xml.key
+cat <<EOF | tee /etc/yum.repos.d/cri-o.repo
+[cri-o]
+name=CRI-O
+baseurl=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/addons:/cri-o:/stable:/$CRIO_VERSION/rpm/repodata/repomd.xml.key
 EOF
 
     # Install package dependencies from the official repositories
